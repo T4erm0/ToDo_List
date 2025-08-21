3c31f13742d725f42a705f6df08c1678b71609e3
UltraSonic272 <vinnig.2011@gmail.com>
2025-08-21 17:51:24 -0400

    Took out errors

--- Project/main.py
+++ Project/main.py
@@ -5,10 +5,15 @@ print("What would you like to do?")
 print("1. Add a task")
 print("2. Remove a task")
 
-user_input = int(input())
+task = []
+
+user_input = int(input("Enter your choice: "))
 
 if user_input == 1:
     task = add_tasks()
-
-print(task)
-
+num_index = len(task)
+if task != "":
+    for i in range(num_index):
+        print(task[i])
+else:
+    print("You did not enter a task")
